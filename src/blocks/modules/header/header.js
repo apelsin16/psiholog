43a49c93ae5bbf8680e3aac97cf59bc470779eda
Apelsin16 <apelsin@i.ua>
2019-11-08 11:15:47 +0200

    add form script

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -2,55 +2,43 @@
 
     const openForm = e => {
         const buttonRecall = document.getElementById('recall');
-        const circle = document.querySelectorAll('.circle');
+        const circleRecall = document.querySelector('.recall .circle');
         const recallForm = document.querySelector('.recall');
+        const buttonsMoreDetails = document.querySelectorAll('.article__button');
+        const formMoreDetails = document.querySelector('.more-details');
+        const circlesMoreDetails = document.querySelector('.more-details .circle');
+        const suitable = document.querySelector('.suitable');
+        const inputHidden = document.getElementById('inputHidden');
         
-        if(e.target === buttonRecall) (
-            recallForm.classList.toggle('d-block')
-        )        
-
-        circle.forEach(el => {
-            el.addEventListener('click', () => {       
-                    recallForm.classList.remove('d-block')
-            })
+        if(e.target === buttonRecall) {
+            recallForm.classList.toggle('d-block');
+            circleRecall.addEventListener('click', () => {       
+                        recallForm.classList.remove('d-block');
+            })            
+        }  
+        
+        buttonsMoreDetails.forEach( el => {
+            if (e.target === el) {
+                const suitableCoord = suitable.getBoundingClientRect();
+                const coord = el.getBoundingClientRect();
+                const number = coord.top - suitableCoord.top + 60;
+                formMoreDetails.style.top = `${number}px`;
+                formMoreDetails.classList.toggle('d-block');
+                const name = el.getAttribute('name');
+                inputHidden.setAttribute('name', name );
+                
+                circlesMoreDetails.addEventListener('click', () => {
+                    formMoreDetails.classList.remove('d-block');
+                })
+            }
         })
+        
+
+
     }
 
     document.addEventListener('click', openForm );
 
-    // const buttonRecall = document.getElementById('recall');
-    // const recallForm = document.querySelector('.recall');
-    // const circle = document.querySelectorAll('.circle');
-    // const buttonMoreDetails = document.querySelectorAll('.article__button')
-    // const moreDetails = document.querySelector('.more-details');
     
-    // const toggleForm = () => {
-        
-    //     recallForm.classList.toggle('d-block');
-    // }
-    
-    // const closeForm = () => {       
-    //     recallForm.classList.remove('d-block');
-    // }
-
-
-
-    // circle.forEach( el =>
-    //     el.addEventListener('click', closeForm))
-    
-    //     buttonRecall.addEventListener('click', toggleForm);
-
-    // const openFormMoreDetails = () => { 
-    //     moreDetails.classList.toggle('d-block');
-    // }
-
-    // buttonMoreDetails.forEach( el => {
-    //     el.getAttribute('name');
-    //     el.addEventListener('click', openFormMoreDetails)
-    // });
-
-    
-
-
 
 })();
